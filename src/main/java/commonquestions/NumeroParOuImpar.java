0c808d9a6c9d72b5c3598fcfa8ed0e6fd3b3bc83
JeffersonMonteiro <jefferson.monteiro@i2s.pt>
2022-08-26 16:53:22 +0100

    more challenges and common tests

---
+++ src/main/java/commonquestions/NumeroParOuImpar.java
@@ -0,0 +1,17 @@
+package commonquestions;
+
+import java.util.Scanner;
+
+public class NumeroParOuImpar {
+    public static void main(String[] args) {
+        int num;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to check even or odd ");
+        num= sc.nextInt();
+        if(num%2==0){
+            System.out.println("Given number "+num+" is even");
+        }else{
+            System.out.println("Given number "+ num +" is odd");
+        }
+    }
+}
